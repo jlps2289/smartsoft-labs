d2b1891d88ab87c8d14f2644ca83bce0c2640d70
Jose Luis Parra Sánchez <jl.ps2289@gmail.com>
2024-01-31 23:50:15 -0500

    🎨 style(setting): Custom mat styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
 	content: ['./src/**/*.{html,ts}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {
+					50: '#ebffff',
+					100: '#cefdff',
+					200: '#a2f8ff',
+					300: '#63f0fd',
+					400: '#1cdef4',
+					500: '#00bcd4',
+					600: '#039ab7',
+					700: '#0a7a94',
+					800: '#126278',
+					900: '#145265',
+					950: '#063646',
+				},
+			},
+		},
 	},
 	plugins: [],
 };
