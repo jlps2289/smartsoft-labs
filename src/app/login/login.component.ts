8a7456c3b45c032f56ba7bb3b7606bef7f64b7b6
Jose Luis Parra Sánchez <jl.ps2289@gmail.com>
2024-02-01 21:48:38 -0500

    ✨ feature(pages): Finish login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,64 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { Router, RouterLink } from '@angular/router';
+
+// Atoms
+import { FooterComponent } from '@atoms/footer/footer.component';
+
+// Components
+import { FieldMessageErrorsComponent } from '@components/field-message-errors/field-message-errors.component';
+import { MutationStatusComponent } from '@components/mutation-status/mutation-status.component';
+
+// Services
+import { AuthService } from '@services/auth.service';
 
 @Component({
 	standalone: true,
-	imports: [CommonModule, RouterLink],
+	imports: [
+		CommonModule,
+		RouterLink,
+		ReactiveFormsModule,
+		MatButtonModule,
+		MatFormFieldModule,
+		MatIconModule,
+		MatInputModule,
+		FooterComponent,
+		FieldMessageErrorsComponent,
+		MutationStatusComponent,
+	],
 	templateUrl: './login.component.html',
-	styleUrl: './login.component.scss',
-	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-class LoginComponent {}
+class LoginComponent implements OnInit {
+	router = inject(Router);
+	mutation = inject(AuthService).login();
+
+	hide = true;
+	isSubmit = false;
+
+	form: FormGroup = new FormGroup({
+		username: new FormControl(''),
+		password: new FormControl(''),
+	});
+
+	constructor(private fb: FormBuilder) {}
+
+	ngOnInit(): void {
+		this.form = this.fb.group({
+			username: ['jose.parra@smartsoftlabs.com', [Validators.required, Validators.email]],
+			password: ['Qwe@123', [Validators.required]],
+		});
+	}
+
+	onSubmit() {
+		this.isSubmit = true;
+		if (this.form.invalid) return;
+		this.mutation.mutateAsync(this.form.value).then(console.log).catch(console.log);
+	}
+}
 
 export default LoginComponent;
